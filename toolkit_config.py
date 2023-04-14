4614bd5a48e05399f886d4f1d878338a22f6e346
YajieWang0203 <130100329+YajieWang0203@users.noreply.github.com>
2023-04-13 21:56:01 -0400

    Add files via upload

---
+++ toolkit_config.py
@@ -0,0 +1,12 @@
+""" toolkit_config.py
+
+Project configuration file
+
+***do not submit this file***
+"""
+
+import os
+
+PRJDIR = r"/Users/krystalwang/PycharmProjects/pythonProject"
+DATADIR = os.path.join(PRJDIR, 'date')
+
